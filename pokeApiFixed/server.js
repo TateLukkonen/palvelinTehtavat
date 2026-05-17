5b5a8ff0dc550c3af46d5374418c936db52ed047
TateLukkonen <s2401191@edu.bc.fi>
2026-05-17 14:38:56 +0300

    vika pokeApi tehtävä

--- pokeApiFixed/server.js
+++ pokeApiFixed/server.js
@@ -7,8 +7,8 @@ app.use("/pub", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("pokeApi")
-})
+  res.render("pokeApi");
+});
 
 app.get("/", (req, res) => {
   res.render("header");
@@ -20,26 +20,28 @@ app.get("/", (req, res) => {
 
 app.get("/gen/:id", async (req, res) => {
   try {
-    const gen = req.params.id
-    
-    const response = await fetch(`https://pokeapi.co/api/v2/generation/${gen}`)
-    const data = await response.json()
+    const gen = req.params.id;
 
-    const pokemons = data.pokemon_species
+    const response = await fetch(`https://pokeapi.co/api/v2/generation/${gen}`);
+    const data = await response.json();
 
-    res.render("gen", { pokemons })
+    const pokemons = data.pokemon_species;
+
+    res.render("gen", { pokemons });
   } catch (err) {
-    console.error(err)
-    res.send("Error fetching Pokémon")
+    console.error(err);
+    res.send("Error fetching Pokémon");
   }
 });
 
 app.get("/pokemonPage/:id", async (req, res) => {
   try {
-    const pokemonName = req.params.id
+    const pokemonName = req.params.id;
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    const data = await response.json()
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+    );
+    const data = await response.json();
 
     res.render("pokemonPage", { pokemon: data });
   } catch (err) {
@@ -49,18 +51,40 @@ app.get("/pokemonPage/:id", async (req, res) => {
 });
 
 app.post("/genSelected", (req, res) => {
-  const choice = req.body.genSelect
-  
-  res.redirect(`/gen/${choice}`)
-})
+  const choice = req.body.genSelect;
+
+  res.redirect(`/gen/${choice}`);
+});
 
 app.post("/pokemonSelected", (req, res) => {
-  const choice = req.body.pokemonSelect
-  
-  res.redirect(`/pokemonPage/${choice}`)
-})
+  const choice = req.body.pokemonSelect;
 
+  res.redirect(`/pokemonPage/${choice}`);
+});
 
 app.listen(3000, () => {
   console.log("Running on http://localhost:3000");
+});
+
+app.post("/searchPokemon", async (req, res) => {
+  try {
+    const pokemon = req.body.pokemonSearch.toLowerCase();
+
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`,
+    );
+
+    if (!response.ok || pokemon == "") {
+      return res.send("Pokemon not found");
+    }
+
+    res.redirect(`/pokemonPage/${pokemon}`);
+  } catch (err) {
+    console.error(err);
+    res.send("Error searching Pokemon");
+  }
+});
+
+app.post("/goHome", (req, res) => {
+  res.redirect(`/`);
 });
